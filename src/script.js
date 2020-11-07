506416c314f9362be7f6704909397ee88767ac4d
nmartineli <70925825+nmartineli@users.noreply.github.com>
2020-11-07 10:47:20 -0300

    JS update: time and weather descripton

--- src/script.js
+++ src/script.js
@@ -1,7 +1,16 @@
-// Current Time
+// Searchbox
 
-let currentDate = new Date();
 
+function formatDate(timestamp){
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+} 
+let minutes = date.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+} 
 let days = [
   "Sunday",
   "Monday",
@@ -11,20 +20,10 @@ let days = [
   "Friday",
   "Saturday",
 ];
+let day = days[date.getDay()];
+return `${day}, ${hours}:${minutes}`;
 
-let hour = currentDate.getHours();
-let minutes = currentDate.getMinutes();
-let day = days[currentDate.getDay()];
-
-let time = document.querySelector("#date");
-
-if (minutes < 10) {
-  time.innerHTML = `${day}, ${hour}:0${minutes}`;
-} else {
-  time.innerHTML = `${day}, ${hour}:${minutes}`;
 }
-
-// Searchbox
 
 function searchDefaultCity(city){
 let apiKey = "6d9cebf9851ed0f6ae8a89e4056f4b5a";
@@ -48,8 +47,8 @@ function getCityInfo(response) {
   document.querySelector("#city").innerHTML = `${response.data.name}, ${response.data.sys.country}`;
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed * 3.6);
  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
- document.querySelector("#description").innerHTML = response.data.weather[0].main;
- 
+ document.querySelector("#description").innerHTML = response.data.weather[0].description;
+ document.querySelector("#date").innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let citySearchButton = document.querySelector("#searchCityButton");
@@ -61,6 +60,16 @@ citySearchBox.addEventListener("submit", searchCity);
 searchDefaultCity("São Paulo")
 
 // Temperature scale
+
+function getFahrenheitTemperature(event) {
+event.preventDefault()
+let celsiusTemperature = document.querySelector("#temperature-number");
+let fahrenheitTemperature = 1 * 1.8 + 32;
+document.querySelector("#temperature-number").innerHTML = Math.round(fahrenheitTemperature);
+}
+
+let fahrenheitScale = document.querySelector("#fahrenheit");
+fahrenheitScale.addEventListener("click", getFahrenheitTemperature)
 
 // Current City
 
