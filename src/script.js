10c91caa61597a93997b3bedc555f9e2eb4e8025
nmartineli <70925825+nmartineli@users.noreply.github.com>
2020-11-07 13:00:05 -0300

    Icon API integration

--- src/script.js
+++ src/script.js
@@ -49,7 +49,8 @@ function getCityInfo(response) {
  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
  document.querySelector("#description").innerHTML = response.data.weather[0].description;
  document.querySelector("#date").innerHTML = formatDate(response.data.dt * 1000);
- document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/10d@2x.png`)
+ document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+document.querySelector("#icon").setAttribute("alt", response.data.weather[0].description);
 }
 
 let citySearchButton = document.querySelector("#searchCityButton");
